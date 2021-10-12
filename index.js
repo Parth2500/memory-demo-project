2129dc651d28bbe0e11e6d550d926654c9df67d3
Parth2500 <pmakawana257@gmail.com>
2021-10-12 14:05:11 +0530

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -18,7 +17,7 @@ app.get('/',(req, res) => {
   res.send('Hello');
 });
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
